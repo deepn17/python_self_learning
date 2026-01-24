f8f4987397841e726bb543085b8f2c4ef53e6afe
deepn17 <70444944+deepn17@users.noreply.github.com>
2026-01-25 02:20:09 +0530

    Enhancement: Refactor calculator to use class with interactive menu

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,49 +1,54 @@
+class Calculator:
 
-def add(a, b):
-    """Add two values using the '+' operator"""
-    return a + b
+    def show_menu(self):
+        print("\n Calculator Menu")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
 
-def subtract(a, b):
-    """Subtract two values using the '-' operator"""
-    return a - b
+    def add(self,a, b):
+        """Add two values using the '+' operator"""
+        return a + b
 
-def multiply(a, b):
-    """Multiply two values using the '*' operator"""
-    return a * b
+    def subtract(self,a, b):
+        """Subtract two values using the '-' operator"""
+        return a - b
 
-def divide(a, b):
-    """Divide two values using the '/' operator"""
-    if b == 0:
-        return "Cannot divide by Zero"
-    return a / b
+    def multiply(self,a, b):
+        """Multiply two values using the '*' operator"""
+        return a * b
 
-def show_menu():
-    print("\n Calculator Menu")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Exit")
+    def divide(self, a, b):
+        """Divide two values using the '/' operator"""
+        if b == 0:
+            return "Cannot divide by Zero"
+        return a / b
+
+    def run(self):
+        while True:
+            self.show_menu()
+            choice = input("Choose an option (1-5): ")
+
+            if choice == '5':
+                print("Goodbye!")
+                break
+
+            a = float(input("Enter the first number: "))
+            b = float(input("Enter the second number: "))
+
+            if choice == '1':
+                print("Result:", self.add(a, b))
+            elif choice == '2':
+                print("Result:", self.subtract(a, b))
+            elif choice == '3':
+                print("Result:", self.multiply(a, b))
+            elif choice == '4':
+                print("Result:", self.divide(a, b))
+            else:
+                print("Invalid choice")
 
 
-while True:
-    show_menu()
-    choice = input("Choose an option (1-5): ")
-
-    if choice == '5':
-        print("Goodbye!")
-        break
-
-    a = float(input("Enter the first number: "))
-    b = float(input("Enter the second number: "))
-
-    if choice == '1':
-        print("Result:", add(a, b))
-    elif choice == '2':
-        print("Result:", subtract(a, b))
-    elif choice == '3':
-        print("Result:", multiply(a, b))
-    elif choice == '4':
-        print("Result:", divide(a, b))
-    else:
-        print("Invalid choice")
+calc = Calculator()
+calc.run()
